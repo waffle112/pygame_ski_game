b51704f7c0e5aeb0cc141bedb271eb4d63ee101a
justin <justinlin920@gmail.com>
2022-04-22 03:07:13 -0700

    added sprites

--- python3_tue/space_invaders.py
+++ python3_tue/space_invaders.py
@@ -71,6 +71,7 @@ barrier_sprites = pygame.sprite.Group()
 
 #sprite sheet
 spritesheet = pygame.image.load('space_invaders.png')
+scale = 3
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -79,8 +80,14 @@ class Player(pygame.sprite.Sprite):
 
         # create an image of the block and fill it with a color
         # this could also be an image loaded from the disk
-        self.image = pygame.Surface([40, 30])
-        self.image.fill(white)
+
+        # self.image = pygame.Surface([40, 30])
+        # self.image.fill(white)
+
+        #cannon - 277, 228 (26px by 16px))
+        self.image = pygame.transform.scale(spritesheet.subsurface(277, 228, 26, 16), (26*scale, 16*scale))
+
+
         # self.image = pygame.image.load("basket.png").convert()
         self.image.set_colorkey((0, 0, 0))
         # Fetch the rectangle object that has the dimensions of the image
@@ -126,7 +133,12 @@ class Player(pygame.sprite.Sprite):
         if self.rect.left <= 0:
             self.rect.left = 0
 
-
+'''
+aliens are 24px by 16px
+        alien1 - 3, 225 -- 36, 225
+        alien2 - 73, 225 -- 106, 225
+        alien3 - 147, 226 -- 179, 226
+'''
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, w, h, x, y):
         # call the parent class (Sprite) constructor
@@ -134,8 +146,12 @@ class Enemy(pygame.sprite.Sprite):
 
         # create an image of the block and fill it with a color
         # this could also be an image loaded from the disk
-        self.image = pygame.Surface([w, h])
-        self.image.fill(white)
+
+        # self.image = pygame.Surface([w, h])
+        # self.image.fill(white)
+
+        self.image = pygame.transform.scale(spritesheet.subsurface(3, 225, 24, 16), (24*scale, 16*scale))
+
         # self.image = pygame.image.load("basket.png").convert()
         self.image.set_colorkey((0, 0, 0))
         # Fetch the rectangle object that has the dimensions of the image
@@ -146,6 +162,8 @@ class Enemy(pygame.sprite.Sprite):
         # initalize speed on x and y axis for sprite
         self.x_speed = 60
         self.y_speed = 0
+
+        self.internal = False
 
     def handle_movement(self):
         self.rect.x += self.x_speed
@@ -170,6 +188,19 @@ class Enemy(pygame.sprite.Sprite):
         else:
             self.handle_movement()
 
+        if self.internal:
+            self.image = pygame.transform.scale(spritesheet.subsurface(3, 225, 24, 16), (24*scale, 16*scale))
+        else:
+            self.image = pygame.transform.scale(spritesheet.subsurface(36, 225, 24, 16), (24*scale, 16*scale))
+        self.internal = not self.internal
+'''
+        barrier1 - 316, 213
+        barrier2 - 373, 211
+        barrier3 - 428, 210
+        barrier4 - 480, 210
+        barrier5 - 480, 265
+        (44 by 32px)
+'''
 class Barrier(pygame.sprite.Sprite):
     def __init__(self, x, y):
         # call the parent class (Sprite) constructor
@@ -177,8 +208,12 @@ class Barrier(pygame.sprite.Sprite):
 
         # create an image of the block and fill it with a color
         # this could also be an image loaded from the disk
-        self.image = pygame.Surface([100, 80])
-        self.image.fill(white)
+
+        # self.image = pygame.Surface([100, 80])
+        # self.image.fill(white)
+
+        self.image = pygame.transform.scale(spritesheet.subsurface(316, 213, 44, 32), (44*scale, 32*scale))
+
         # self.image = pygame.image.load("basket.png").convert()
         self.image.set_colorkey((0, 0, 0))
         # Fetch the rectangle object that has the dimensions of the image
@@ -195,10 +230,17 @@ class Barrier(pygame.sprite.Sprite):
     # update method
     def update(self):
         #check if hp is below or at 0 then self.kill
-        if self.hp <= 0:
+
+        if self.hp >= 20:
+            self.image = pygame.transform.scale(spritesheet.subsurface(316, 213, 44, 32), (44*scale, 32*scale))
+        elif self.hp >= 20 * .8:
+            self.image = pygame.transform.scale(spritesheet.subsurface(480, 210, 44, 32), (44*scale, 32*scale))
+        elif self.hp >= 20 * .6:
+            self.image = pygame.transform.scale(spritesheet.subsurface(373, 211, 44, 32), (44*scale, 32*scale))
+        elif self.hp >= 20 * .4:
+            self.image = pygame.transform.scale(spritesheet.subsurface(428, 210, 44, 32), (44*scale, 32*scale))
+        else:
             self.kill()
-
-
 '''
 need 2 bullets 
 one for the player 
@@ -237,6 +279,42 @@ class bullet1(pygame.sprite.Sprite):
         # if the bullet goes out of bounds -> delete bullet
         if self.rect.bottom <= 0:
             self.kill()
+
+class button(pygame.sprite.Sprite):
+
+    def __init__(self, color, x, y, w, h, text=''):
+        # call the parent class (Sprite) constructor
+        pygame.sprite.Sprite.__init__(self)
+        #elf.rect = self.image.get_rect()
+        #self.rect.center = (x, y)
+        self.clicked = False # include new variable
+        self.color = color
+        self.x = x
+        self.y = y
+        self.width = w
+        self.height = h
+        self.text = text
+
+    def draw(self, outlineColor=None):
+        if outlineColor:
+            pygame.draw.rect(screen, outlineColor, (self.x - 2, self.y - 2, self.width + 4, self.height + 4))
+
+        # if you have pics instead of buttons -> load an image instead of drawing a rect
+        #screen.blit(self.image, (self.rect.x, self.rect.y))
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
+
+        if self.text != '':
+            font = pygame.font.Font(arial, 18)
+            text_surface = font.render(self.text, True, white)
+            screen.blit(text_surface, (self.x + (self.width / 2 - text_surface.get_width() / 2),
+                                       self.y + (self.height / 2 - text_surface.get_height() / 2)))
+        pressed = False
+        pos = pygame.mouse.get_pos()
+        if self.x < pos[0] < self.x + self.width and self.y < pos[1] < self.y + self.height:
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                pressed = True
+        return pressed
 
 def draw_text(surf, text, size, x, y):
     font = pygame.font.Font(arial, size)
@@ -287,11 +365,62 @@ def alienSideCheck():
 
 def game_over():
     print("Game Over")
+
+    #create two buttons color, x, y, width, height, text=''
+    buttonQuit = button(black, width/2-50, height/4 + 60, 100, 50, "Quit Game")
+    buttonRestart = button(black, width/2-50, height/4 , 100, 50, "Restart Game")
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
-def game():
-    player = Player()
+
+
+        screen.fill(black)
+        if buttonRestart.draw(white):
+            #seems like sprites arent deleted yet
+            all_sprites.empty()
+            enemy_sprites.empty()
+            bullet_sprites.empty()
+            alien_b_sprites.empty()
+            barrier_sprites.empty()
+            game()
+
+        if buttonQuit.draw(white):
+            sys.exit()
+
+        pygame.display.flip()
+
+def start_game():
+    print("Game Start")
+
+    #create two buttons color, x, y, width, height, text=''
+    buttonQuit = button(black, width/2-50, height/4+ 60, 100, 50, "Quit Game")
+    buttonStart = button(black, width/2-50, height/4, 100, 50, "Start Game")
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+
+        screen.fill(black)
+        if buttonStart.draw(white):
+            break
+
+        if buttonQuit.draw(white):
+            sys.exit()
+
+        pygame.display.flip()
+
+def game(win=None, hp = None, newspeed = None):
+    if win is not None:
+        player = Player()
+        player.score = win
+        player.hp = hp
+        speed = newspeed
+    else:
+        start_game()
+        player = Player()
+        speed = 2000
+
     all_sprites.add(player)
     for y in range(1, 6):
         for x in range(1, 11):
@@ -313,6 +442,9 @@ def game():
     pygame.time.set_timer(pygame.USEREVENT + 3, 800)
 
     firing_delay = False
+
+    win = 0
+
     while True:
         clock.tick(60)  # set FPS to 60 FPS
         for event in pygame.event.get():
@@ -327,7 +459,7 @@ def game():
                     bullet_sprites.add(b)
                     all_sprites.add(b)
                     firing_delay = True
-                    pygame.time.set_timer(pygame.USEREVENT + 2, 1000)
+                    pygame.time.set_timer(pygame.USEREVENT + 2, 200)
 
             if event.type == pygame.USEREVENT + 1:
                 # move the enemies
@@ -336,7 +468,7 @@ def game():
                     enemy_sprites.update("left")
                 if leftmost.rect.left <= 0:
                     enemy_sprites.update("right")
-                pygame.time.set_timer(pygame.USEREVENT + 1, 2000)
+                pygame.time.set_timer(pygame.USEREVENT + 1, speed)
 
             if event.type == pygame.USEREVENT + 2:
                 firing_delay = False
@@ -360,6 +492,12 @@ def game():
             h.hp -= 1
         hits = pygame.sprite.groupcollide(barrier_sprites, alien_b_sprites, False, True)
         for h in hits:
+            print("barier hit")
+            h.hp -= 1
+
+        hits = pygame.sprite.groupcollide(barrier_sprites, bullet_sprites, False, True)
+        for h in hits:
+            print("barrier hit")
             h.hp -= 1
 
         hits = pygame.sprite.groupcollide(bullet_sprites, enemy_sprites, True, True)
@@ -385,9 +523,25 @@ def game():
         show_score(player.score)
         pygame.display.flip()
 
-        if player.hp <= 0:
-            game_over()
 
+
+        #check if game over
+        if player.hp <= 0:
+            win = None
+            break
+        if not enemy_sprites:
+            #respawn all the enemies and increase hp by 1
+            win = player.score
+            player.hp += 1
+            break
+
+    if win is not None:
+        player.kill()
+        for b in barrier_sprites:
+            b.kill()
+        game(win, player.hp, int(speed*.95))
+    else:
+        game_over()
 
 if __name__ == "__main__":
     game()
